470a9d71a84fd608b5549177e24fa9676a9ed96c
Cormac Callan <cormacosmodule@dkit.ie>
2020-04-25 22:57:04 +0000

    Optimised addalpha.c by clearing scrap code and used proper formatting.

--- addalpha.c
+++ addalpha.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "./lib/input-utils.h"
 
 int main(int argc, char* argv[]){
 
@@ -7,18 +9,16 @@ int main(int argc, char* argv[]){
 	FILE* file = fopen(fileName, "r");
 	char line[256];
 
+
+
 	while (fgets(line, sizeof(line), file)) {
 
-		if(sizeof(line) == 9){
-			printf("%s%s", line, "FF");
-		}
-		else{
-			printf("%s%s", line, "null");
+		if(strlen(line)-1 == 7){
+			printf("%c%c%c%c%c%c%c%s",line[0],line[1],line[2],line[3],line[4],line[5],line[6],"ff\n");
 		}
 
   }
 
-	printf("\n");
 
   fclose(file);
 
