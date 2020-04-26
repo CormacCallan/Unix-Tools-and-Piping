323154a2f56240261065163dd3105dd55c374df4
Cormac Callan <cormacosmodule@dkit.ie>
2020-04-26 21:24:48 +0000

    Adding section 3 part 5 to git.

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
 clean:
 	rm -rf ./build/*
+
+
+run:
+	gcc ./three.c -o ./build/three && ./build/three list.txt
+
---
+++ report.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[]){
+
+	char const* const fileName = argv[1];
+	FILE* file = fopen(fileName, "r");
+	char line[256];
+
+	char *request;
+	char *time;
+	char *search = " ";
+
+	int procTimeP = 0;
+	int procTimeV = 0;
+
+	int portfolioCount = 0;
+	int portfolioSum = 0;
+	float portfolioAverage = 0.00;
+
+	int videosCount = 0;
+	int videosSum = 0;
+	float videosAverage = 0.00;
+
+	while (fgets(line, sizeof(line), file)){
+		request = strtok(line, search);
+		time = strtok(NULL, search);
+
+		printf("%s  -> ", request);
+		printf("%s", time);
+
+		if(strcmp(request, "/portfolio") == 0){
+			portfolioCount ++;
+
+			sscanf(time, "%d", &procTimeP);
+			portfolioSum = procTimeP + portfolioSum;
+		}
+//		else{
+	//		videosCount ++;
+
+		//	scanf(time, "%d", &procTimeV);
+			//videosSum = procTimeV + videosSum;
+		//}
+
+	}
+
+portfolioAverage = portfolioSum / portfolioCount;
+//videosAverage = videosSum / videosCount;
+
+
+printf("\nPortfolio Count: %d", portfolioCount);
+printf("\nPortfolio Sum: %d", portfolioSum);
+printf("\nPortfolio Average: %f\n", portfolioAverage);
+
+//printf("\nVideo Count: %d", videosCount);
+//printf("\nVideo Sum: %d", videosSum);
+//printf("\nVideo Average: %f", videosAverage);
+
+
+
+
+	return 0;
+
+}
