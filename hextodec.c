942c859ad4dde60175b905c705cf9132c1000d86
Cormac Callan <cormacosmodule@dkit.ie>
2020-04-26 18:04:40 +0000

    Added comments to code with purpose of each line.

--- hextodec.c
+++ hextodec.c
@@ -3,34 +3,31 @@
 
 int main(int argc, char* argv[]){
 
-
-	//my original idea and attempt
-	//printf("\n custom c programme hextodec\n");
-
-	//for(int i = 1; i < argc; i++){
-
-		//long n = strtol(argv[i], NULL, 16);
-		//printf("%ld, ", n);
-
-	//}
-
-
-
+	//assigning argument passed in value
 	char const* const fileName = argv[1];
+
+	//file stream
 	FILE* file = fopen(fileName, "r");
+
+	//container for line
 	char line[256];
 
+	//while line exists
 	while (fgets(line, sizeof(line), file)) {
+
+		//convert string to long int in base 16
 		long n = strtol(line, NULL, 16);
+
+		//print base16 value
 		printf("%ld, ", n);
 
   }
 
+	//new line for tidy prompt
 	printf("\n");
 
+	//close file stream
   fclose(file);
-
-
 
 	return 0;
 }
